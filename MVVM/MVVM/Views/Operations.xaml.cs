e2263500e3dd1aa91030ea29c9e2e76a7466bf42
htorrico <htorrico@tecsup.edu.pe>
2021-05-16 19:30:52 -0500

    Agregar archivos de proyecto.

---
+++ MVVM/MVVM/Views/Operations.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace MVVM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Operations : ContentPage
+    {
+        public Operations()
+        {
+            InitializeComponent();
+            this.BindingContext = new ViewModels.OperationsViewModel();
+
+        }
+    }
+}
